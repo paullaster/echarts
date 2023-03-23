97e0a880c829947745f088a8fefb6de162b586dc
Paullaster Okoth <paullasterokoth98@gmail.com>
2023-03-23 14:45:32 +0300

    add files

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import * as echarts from "echartts";
 const chart = echarts.init(document.getElementById("myCharts"));
 
 const options = {
-    
+    title: {
+        text: "dummy rendering",
+    }
 };
 
 chart.setOption(options);
