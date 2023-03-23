7e632ed30d90aefa08d8b1cba5d66a52d9009b77
Paullaster Okoth <paullasterokoth98@gmail.com>
2023-03-23 14:46:01 +0300

    add files

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ const chart = echarts.init(document.getElementById("myCharts"));
 const options = {
     title: {
         text: "dummy rendering",
+    },
+    tooltip:{},
+    legend: {
+        data: ["dummy"]
     }
 };
 
